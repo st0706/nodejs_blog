7641533613890d258037836aae0ea2cd2180faca
Nguyen Son Tung <1stgemini123@gmail.com>
2023-07-18 14:04:54 +0700

    blog

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -7,6 +7,7 @@ class SiteController {
     index(req, res, next) {
         const defaultAvatar = 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'
         let data = {};
+        const PAGE_SIZE = 3;
         if (req.session.username) {
             User.findOne({ username: req.session.username })
                 .then(user => {
@@ -16,15 +17,46 @@ class SiteController {
                     return Blog.find({}).populate('author');
                 })
                 .then(blogs => {
-                    data.blogs = multipleMongooseToObject(blogs)
-                    res.render('home', data)
+                    let page = parseInt(req.query.page) || 1;
+                    const totalBlogs = blogs.length;
+                    const pageCount = Math.ceil(totalBlogs / PAGE_SIZE);
+                    const startIndex = (page - 1) * PAGE_SIZE;
+                    const endIndex = Math.min(startIndex + PAGE_SIZE, totalBlogs);
+
+                    data.blogs = multipleMongooseToObject(blogs.slice(startIndex, endIndex));
+                    data.page = page;
+                    data.pages = [];
+
+                    for (let i = 1; i <= pageCount; i++) {
+                        data.pages.push({
+                            page: i,
+                            active: i === page
+                        });
+                    }
+                    data.pageCount = pageCount;
+                    res.render('home', data);
                 })
                 .catch(next);
         } else {
             Blog.find({}).populate('author')
                 .then(blogs => {
                     data.isLoginView = false;
-                    data.blogs = multipleMongooseToObject(blogs);
+                    let page = parseInt(req.query.page) || 1;
+                    const totalBlogs = blogs.length;
+                    const pageCount = Math.ceil(totalBlogs / PAGE_SIZE);
+                    const startIndex = (page - 1) * PAGE_SIZE;
+                    const endIndex = Math.min(startIndex + PAGE_SIZE, totalBlogs);
+                    data.blogs = multipleMongooseToObject(blogs.slice(startIndex, endIndex));
+                    data.page = page;
+                    data.pages = [];
+
+                    for (let i = 1; i <= pageCount; i++) {
+                        data.pages.push({
+                            page: i,
+                            active: i === page
+                        });
+                    }
+                    data.pageCount = pageCount;
                     res.render('home', data);
                 })
                 .catch(next)
